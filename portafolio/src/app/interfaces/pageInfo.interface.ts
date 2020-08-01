ea9f57780b279281021518c2614183d205409a38
Juan Manuel Mena Hernández <55758232+jmmenah@users.noreply.github.com>
2020-08-01 23:29:20 +0200

    Add info interface

---
+++ portafolio/src/app/interfaces/pageInfo.interface.ts
@@ -0,0 +1,11 @@
+export interface PageInfo {
+  title?: string;
+  email?: string;
+  shortname?: string;
+  authorpage?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tumblr?: string;
+  workteam?: any[];
+}
---
+++ portafolio/src/app/services/page-info.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PageInfo } from '../interfaces/pageInfo.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PageInfoService {
+
+  info: PageInfo = {};
+  charged = false;
+
+  constructor( private http: HttpClient) {
+    // console.log('InfoPage works!');
+
+    // read JSON
+    this.http.get('assets/data/data-page.json').subscribe( resp: PageInfo => {
+
+      this.charged = true;
+      this.info = resp;
+      console.log(resp);
+
+    });
+
+
+   }
+}
