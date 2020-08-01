bc6bf897b88162239c28f754b400cacc0947e895
Juan Manuel Mena Hernández <55758232+jmmenah@users.noreply.github.com>
2020-08-01 21:51:42 +0200

    Add routing

--- portafolio/src/app/app-routing.module.ts
+++ portafolio/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ItemComponent } from './pages/item/item.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: PortafolioComponent},
+  {path:'about', component: AboutComponent},
+  {path:'item', component: ItemComponent},
+  {path: '**', pathMatch: 'full', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+ }
